c680a984213cadbbabb974ae9eb05270fcd624dd
Grace Bokunde <123752918+GraceBokunde@users.noreply.github.com>
2023-04-04 21:52:46 +0100

    Update assign02.c

--- assign02.c
+++ assign02.c
@@ -1,10 +1,74 @@
-// Must declare the main assembly entry point before use.
-void main_asm();
+#include <stdio.h>
+//#include "pico/stdlib.h"
+//#include "hardware/gpio.h"
 
-/*
- * Main entry point for the code - simply calls the main assembly function.
- */
+
+
+void welcome() 
+
+{
+
+  
+  printf("\n+____________________________________________________________________________________+\n");
+  printf("|                        Group 44                            |                       |\n");
+  printf("|------------------------------------------------------------+-----------------------|\n");
+  printf("|                                                            |      GAME LEVELS      |\n");
+  printf("|                                                            |-----------------------|\n");
+  printf("|                                                            |    LEVEL 1 - EASY     |\n");
+  printf("|                                                            |    LEVEL 2 - MEDIUM   |\n");
+  printf("|                                                            |    LEVEL 3 - HARD     |\n");
+  printf("|                                                            |    LEVEL 4 - EXTREME  |\n");
+  printf("|                                                            |-----------------------|\n");
+  printf("|                                                            |                       |\n");
+  printf("|                                                            |                       |\n");
+  printf("|                                                            |                       |\n");
+  printf("|                                                            |                       |\n");
+  printf("|                                                            |                       |\n");
+  printf("|                                                            |                       |\n");
+  printf("|                                                            |                       |\n");
+  printf("|                                                            |                       |\n");
+  printf("|                                                            |                       |\n");
+  printf("|                                                            |                       |\n");
+  printf("|                                                            |                       |\n");
+  printf("|                                                            |                       |\n");
+  printf("|                                                            |                       |\n");
+  printf("|                                                            |                       |\n");
+  printf("|                                                            |                       |\n");
+  printf("|                                                            |                       |\n");
+  printf("+____________________________________________________________+_______________________+\n");
+
+}
+
+void endGame()
+{ 
+  printf("\n+____________________________________________________________+\n");
+  printf("|                                                            |\n");
+  printf("|        ########        #      #       #   ########         |\n");
+  printf("|        #             #   #    # #   # #   #                |\n");
+  printf("|        #           #       #  #  # #  #   #                |\n");
+  printf("|        #    ###    #########  #   #   #   ########         |\n");
+  printf("|        #      #    #       #  #       #   #                |\n");
+  printf("|        #      #    #       #  #       #   #                |\n");
+  printf("|        ########    #       #  #       #   ########         |\n");
+  printf("|                                                            |\n");
+  printf("|         #######     #       #  #########   #######     #   |\n"),
+  printf("|        #       #    #       #  #           #      #    #   |\n");
+  printf("|        #       #    #       #  #           #      #    #   |\n");
+  printf("|        #       #    #       #  #########   # # # #     #   |\n");
+  printf("|        #       #    #       #  #           #  #        #   |\n");
+  printf("|        #       #     #     #   #           #    #          |\n");
+  printf("|         #######         #      ##########  #      #    #   |\n");
+  printf("|                                                            |\n");
+  printf("+____________________________________________________________+\n");
+
+
+}
+
+// Main entry point of the application
 int main() {
-    main_asm();
-    return(0);
+    //stdio_init_all();              // Initialise all basic IO
+    welcome();
+    endGame();
+    //main_asm();                    // Jump into the ASM code
+    return 0;                      // Application return code
 }
